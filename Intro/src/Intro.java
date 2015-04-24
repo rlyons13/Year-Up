2685ddf7da4bd1a969b11012828e38034bfb6147
rlyons13 <rlyons@atl.yearup.org>
2015-04-24 15:39:20 -0400

    First Java Project

---
+++ Intro/src/Intro.java
@@ -0,0 +1,16 @@
+ 
+public class Intro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello! Welcome to Java World");
+				
+	}
+ /*
+  * syntax error, insert; to complete BlockStatements
+  * Syntax error, insert ) to complete Expression
+  */
+
+}
+
+
